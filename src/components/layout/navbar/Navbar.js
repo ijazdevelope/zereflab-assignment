80a277bb38c956037e6bb13454bbc93d96f98c70
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-05-13 17:30:40 +0500

    navbar completed and make it responsive on every devices

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import './navbar.scss'
+import Search from '../../../assets/images/search.png';
+import Notification from '../../../assets/images/notification.png';
+import Avatar from '../../../assets/images/avatar.png';
 
 const Index = () => {
   return (
-    <div>
-      navbar.js
-    </div>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Registration</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+            <li className="nav-item">
+              <a className="nav-link p-0 active" aria-current="page" href="#">
+                <img src={Search} alt="navbar-search-icon" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link p-0" href="#">
+                <img src={Notification} alt="navbar-notification-icon" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link p-0" href="#">
+                <img src={Avatar} alt="navbar-avatar-icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
